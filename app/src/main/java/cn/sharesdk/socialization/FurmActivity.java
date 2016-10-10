2d2b5c5e3783fa41012b000b107d67bb01f2c194
jjdxmashl <316988670@qq.com>
2016-10-11 00:31:44 +0800

    接入mob评论

---
+++ app/src/main/java/cn/sharesdk/socialization/FurmActivity.java
@@ -0,0 +1,244 @@
+/*
+ * 官网地站:http://www.mob.com
+ * 技术支持QQ: 4006852216
+ * 官方微信:ShareSDK   （如果发布新版本的话，我们将会第一时间通过微信将版本更新内容推送给您。如果使用过程中有任何问题，也可以通过微信与我们取得联系，我们将会在24小时内给予回复）
+ *
+ * Copyright (c) 2013年 mob.com. All rights reserved.
+ */
+
+package cn.sharesdk.socialization;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Handler.Callback;
+import android.os.Message;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
+
+import com.hyphenate.chatuidemo.R;
+import com.mob.tools.utils.UIHandler;
+
+import cn.sharesdk.framework.Platform;
+import cn.sharesdk.framework.Platform.ShareParams;
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
+import cn.sharesdk.onekeyshare.ShareContentCustomizeCallback;
+import cn.sharesdk.socialization.CommentFilter.FilterItem;
+import cn.sharesdk.socialization.component.ReplyTooFrequentlyException;
+import cn.sharesdk.socialization.component.TopicTitle;
+
+import static com.mob.tools.utils.R.getStringRes;
+
+/**
+ * 评论和赞功能的演示页面
+ */
+public class FurmActivity extends Activity implements Callback, OnClickListener {
+    // 模拟的主题id
+    private String topicId;
+    // 模拟的主题标题
+    private String topicTitle;
+    // 模拟的主题发布时间
+    private String topicPublishTime;
+    // 模拟的主题作者
+    private String topicAuthor;
+    private OnekeyShare oks;
+    private QuickCommentBar qcBar;
+    private CommentFilter filter;
+    private Context context;
+
+    private static final int INIT_SDK = 1;
+    private static final int AFTER_LIKE = 2;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_furm);
+        context = FurmActivity.this;
+        ShareSDK.initSDK(this);
+        ShareSDK.registerService(Socialization.class);
+
+        new Thread() {
+            public void run() {
+                UIHandler.sendEmptyMessageDelayed(INIT_SDK, 100, FurmActivity.this);
+            }
+        }.start();
+
+        //设置评论监听
+        Socialization.setCommentListener(new CommentListener() {
+
+            @Override
+            public void onSuccess(Comment comment) {
+                int resId = getStringRes(context, "ssdk_socialization_reply_succeeded");
+                if (resId > 0) {
+                    Toast.makeText(context, resId, Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFail(Comment comment) {
+                Toast.makeText(context, comment.getFileCodeString(context), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                if (throwable instanceof ReplyTooFrequentlyException) {
+                    int resId = getStringRes(context, "ssdk_socialization_replay_too_frequently");
+                    if (resId > 0) {
+                        Toast.makeText(context, resId, Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    throwable.printStackTrace();
+                }
+            }
+        });
+
+        Socialization.setLikeListener(new LikeListener() {
+
+            @Override
+            public void onSuccess(String topicId, String topicTitle, String commentId) {
+                Message msg = new Message();
+                msg.what = AFTER_LIKE;
+                msg.arg1 = 1;
+                UIHandler.sendMessage(msg, FurmActivity.this);
+            }
+
+            @Override
+            public void onFail(String topicId, String topicTitle, String commentId, String error) {
+                Message msg = new Message();
+                msg.what = AFTER_LIKE;
+                msg.arg1 = 2;
+                UIHandler.sendMessage(msg, FurmActivity.this);
+            }
+
+        });
+    }
+
+    public boolean handleMessage(Message msg) {
+        switch (msg.what) {
+            case INIT_SDK:
+                topicId = "ssdk20131220190000";
+                topicTitle = "ShareSDK“评论与赞”模块发布";
+                topicPublishTime = "2013-12-20 19:00";
+                topicAuthor = "ShareSDK";
+
+                TopicTitle tt = (TopicTitle) findViewById(R.id.llTopicTitle);
+                tt.setTitle(topicTitle);
+                tt.setPublishTime(topicPublishTime);
+                tt.setAuthor(topicAuthor);
+
+                Socialization service = ShareSDK.getService(Socialization.class);
+                service.setCustomPlatform(new MyPlatform(this));
+                initOnekeyShare();
+                initQuickCommentBar();
+                break;
+            case AFTER_LIKE:
+                if (msg.arg1 == 1) {
+                    //success
+                    int resId = getStringRes(context, "like_success");
+                    if (resId > 0) {
+                        Toast.makeText(context, resId, Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    //fail
+                    int resId = getStringRes(context, "like_fail");
+                    if (resId > 0) {
+                        Toast.makeText(context, resId, Toast.LENGTH_SHORT).show();
+                    }
+                }
+                break;
+            case 3:
+                break;
+            default:
+                break;
+
+        }
+
+        return false;
+    }
+
+    // Socialization服务依赖OnekeyShare组件，此方法初始化一个OnekeyShare对象
+    // 此方法的代码从DemoPage中复制而来
+    private void initOnekeyShare() {
+        oks = new OnekeyShare();
+        oks.setAddress("12345678901");
+        oks.setTitle(getString(R.string.ssdk_oks_share));
+        oks.setTitleUrl("http://mob.com");
+        oks.setText("ShareSDK for Android不仅集成简单、支持如QQ好友、微信、新浪微博、腾讯微博等所有社交平台，而且还有强大的统计分析管理后台，实时了解用户、信息流、回流率、传播效应等数据，详情见官网http://mob.com @ShareSDK");
+        oks.setUrl("http://www.mob.com");
+        oks.setComment(getString(R.string.ssdk_oks_share));
+        oks.setSite(getString(R.string.app_name));
+        oks.setSiteUrl("http://mob.com");
+        oks.setVenueName("ShareSDK");
+        oks.setVenueDescription("This is a beautiful place!");
+        oks.disableSSOWhenAuthorize();
+        oks.setShareContentCustomizeCallback(new ShareContentCustomizeCallback() {
+            public void onShare(Platform platform, ShareParams paramsToShare) {
+                // 改写twitter分享内容中的text字段，否则会超长，
+                // 因为twitter会将图片地址当作文本的一部分去计算长度
+                if ("Twitter".equals(platform.getName())) {
+                    paramsToShare.setText("ShareSDK不仅集成简单、支持如微信、新浪微博、腾讯微博等社交平台，而且还有强大的统计分析管理后台，实时了解用户、信息流、回流率、传播效应等数据，详情见官网http://mob.com @ShareSDK");
+                }
+            }
+        });
+    }
+
+    private void initQuickCommentBar() {
+        qcBar = (QuickCommentBar) findViewById(R.id.qcBar);
+        qcBar.setTopic(topicId, topicTitle, topicPublishTime, topicAuthor);
+        qcBar.setTextToShare("ShareSDK for Android不仅集成简单、支持如QQ好友、微信、新浪微博、腾讯微博等所有社交平台，而且还有强大的统计分析管理后台，实时了解用户、信息流、回流率、传播效应等数据，详情见官网http://mob.com @ShareSDK");
+        qcBar.getBackButton().setOnClickListener(this);
+        qcBar.setAuthedAccountChangeable(false);
+
+        CommentFilter.Builder builder = new CommentFilter.Builder();
+        // 非空过滤器
+        builder.append(new FilterItem() {
+            // 返回true表示是垃圾评论
+            public boolean onFilter(String comment) {
+                if (TextUtils.isEmpty(comment)) {
+                    return true;
+                } else if (comment.trim().length() <= 0) {
+                    return true;
+                } else if (comment.trim().toLowerCase().equals("null")) {
+                    return true;
+                }
+                return false;
+            }
+
+            @Override
+            public int getFilterCode() {
+                return 0;
+            }
+        });
+        // 字数上限过滤器
+        builder.append(new FilterItem() {
+            // 返回true表示是垃圾评论
+            public boolean onFilter(String comment) {
+                if (comment != null) {
+                    String pureComment = comment.trim();
+                    String wordText = com.mob.tools.utils.R.toWordText(pureComment, 140);
+                    if (wordText.length() != pureComment.length()) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+
+            @Override
+            public int getFilterCode() {
+                return 0;
+            }
+        });
+        filter = builder.build();
+        qcBar.setCommentFilter(filter);
+        qcBar.setOnekeyShare(oks);
+    }
+
+    public void onClick(View v) {
+        if (v.equals(qcBar.getBackButton())) {
+            finish();
+        }
+    }
+
+}
--- app/src/main/java/com/hyphenate/chatuidemo/DemoApplication.java
+++ app/src/main/java/com/hyphenate/chatuidemo/DemoApplication.java
@@ -1,10 +1,10 @@
 /**
  * Copyright (C) 2016 Hyphenate Inc. All rights reserved.
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,38 +21,38 @@ import com.easemob.redpacketsdk.RedPacket;
 
 public class DemoApplication extends Application {
 
-	public static Context applicationContext;
-	private static DemoApplication instance;
-	// login user name
-	public final String PREF_USERNAME = "username";
-	
-	/**
-	 * nickname for current user, the nickname instead of ID be shown when user receive notification from APNs
-	 */
-	public static String currentUserNick = "";
+    public static Context applicationContext;
+    private static DemoApplication instance;
+    // login user name
+    public final String PREF_USERNAME = "username";
 
-	@Override
-	public void onCreate() {
-		MultiDex.install(this);
-		super.onCreate();
+    /**
+     * nickname for current user, the nickname instead of ID be shown when user receive notification from APNs
+     */
+    public static String currentUserNick = "";
+
+    @Override
+    public void onCreate() {
+        MultiDex.install(this);
+        super.onCreate();
         applicationContext = this;
         instance = this;
-        
+
         //init demo helper
         DemoHelper.getInstance().init(applicationContext);
-		//red packet code : 初始化红包上下文，开启日志输出开关
-		RedPacket.getInstance().initContext(applicationContext);
-		RedPacket.getInstance().setDebugMode(true);
-		//end of red packet code
-	}
+        //red packet code : 初始化红包上下文，开启日志输出开关
+        RedPacket.getInstance().initContext(applicationContext);
+        RedPacket.getInstance().setDebugMode(true);
+        //end of red packet code
+    }
 
-	public static DemoApplication getInstance() {
-		return instance;
-	}
+    public static DemoApplication getInstance() {
+        return instance;
+    }
 
-	@Override
-	protected void attachBaseContext(Context base) {
-		super.attachBaseContext(base);
-		MultiDex.install(this);
-	}
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+    }
 }
